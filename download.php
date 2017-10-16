0034a0a9d3f3ee28932bdd9d6075af7093de9f68
Arif Budi Setiawan <32523309+arrive93@users.noreply.github.com>
2017-10-17 00:09:01 +0700

    Add files via upload

--- download.php
+++ download.php
@@ -1,7 +1,7 @@
 
 <!DOCTYPE html>
 <html>
-<title>Hasil Tokenisasi STBI</title>
+<title>Cari Documents STBI</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
