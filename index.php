604c0ec994cffcde574f043ef1fbb2e4e68d921e
Arif Budi Setiawan <32523309+arrive93@users.noreply.github.com>
2017-10-19 01:51:57 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -52,6 +52,10 @@ body, h1,h2,h3,h4,h5,h6 {font-family: "Montserrat", sans-serif}
     <i class="fa fa-envelope w3-xxlarge"></i>
     <p>Cari Documents</p>
   </a>
+  <a href="makalah.php" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
+    <i class="fa fa-eye w3-xxlarge"></i>
+    <p>Makalah</p>
+  </a>
 </nav>
 
 <!-- Navbar on small screens (Hidden on medium and large screens) -->
